bfd7f605ddf41add180a52f422afe5859108f86b
YDHouse <eastmerit11@gmail.com>
2020-11-01 16:50:29 +0900

    first

---
+++ front-vue-webpack/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import NaverLogin from '@/components/NaverLogin'
+import TestVue from '@/components/TestVue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,
+    },
+    {
+      path: '/login',
+      name: 'NaverLogin',
+      component: NaverLogin,
+    },
+    {
+      path: '/test',
+      name: 'TestVue',
+      component: TestVue,
+    },
+  ],
+})
